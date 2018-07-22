34b6e03f63363ccc416dc2bac44156964b380574
Sean Christopher Febo <scf@techie.com>
2018-07-22 21:58:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,21 @@ function postComment() {
 }
 
 function createPost() { 
-  
+  let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  let postTemplate = _.template(document.getElementById("post-template").innerHTML);
++	var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
++
++	// Blog values
++	var postTitle = document.getElementById("postTitle").value;
++	var postAuthor = document.getElementById("postAuthor").value;
++	var postContent = document.getElementById("postContent").value;
++
++	// append, not replace!
++	document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
++
++	const blogSection = postTemplate({"title": postTitle, "content": postContent, "author": postAuthor});
++	const commentsSection = commentsTemplate();
++	const postElement = document.getElementById("post");
++
++	postElement.innerHTML = blogSection;
++	postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
